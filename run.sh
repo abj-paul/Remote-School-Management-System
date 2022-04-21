0e1e17008b0b99892c46dddd83a76856bb318e3b
Abhijit Paul <bsse1201@iit.du.ac.bd>
2022-04-21 17:53:43 +0600

    Updating run script. Because compiling Client raises error

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-javac -d bin/ src/backend/institution/*.java src/backend/student/*.java src/backend/teacher/*.java src/backend/generalClasses/*.java src/backend/result/*.java src/backend/exceptions/*.java src/backend/parser/*.java src/networking/*.java
+javac -d bin/ src/backend/institution/*.java src/backend/student/*.java src/backend/teacher/*.java src/backend/generalClasses/*.java src/backend/result/*.java src/backend/exceptions/*.java src/backend/parser/*.java src/networking/Server.java
 echo "Done compiling!"
 cd bin/
 java backend.parser.Parser
